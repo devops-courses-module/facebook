d2dfb0276a146f33ef30945a9c95e4e119383851
Bhavya <bhavyanaramaneni72@gmail.com>
2025-12-09 11:34:18 +0530

    conflict is resolves

--- StringUtils.java
+++ StringUtils.java
@@ -4,8 +4,5 @@ i am creating a new branch
 I am learning devops
 This is my second module
 .....
-<<<<<<< HEAD
 I am updating the file again.....
-=======
 I am updating the file again in the development branch..
->>>>>>> development
